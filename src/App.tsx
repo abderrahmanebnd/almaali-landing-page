ffda0cf94ab62f303ff6c106d601fbae9196bceb
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-08-04 23:49:13 +0100

    remove admin pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ import Index from "./pages/Index";
 import Courses from "./pages/Courses";
 import Teachers from "./pages/Teachers";
 import NotFound from "./pages/NotFound";
-import AdminLayout from "./components/admin/AdminLayout";
-import Dashboard from "./pages/admin/Dashboard";
-import AdminStudents from "./pages/admin/Students";
-import AdminTeachers from "./pages/admin/Teachers";
-import AdminCourses from "./pages/admin/Courses";
-import Registrations from "./pages/admin/Registrations";
 
 const queryClient = new QueryClient();
 
@@ -26,16 +20,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/teachers" element={<Teachers />} />
-          
-          {/* Admin Routes */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="students" element={<AdminStudents />} />
-            <Route path="teachers" element={<AdminTeachers />} />
-            <Route path="courses" element={<AdminCourses />} />
-            <Route path="registrations" element={<Registrations />} />
-          </Route>
-          
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
