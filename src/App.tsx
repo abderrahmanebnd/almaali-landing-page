b3fce912701f9b2669da9700cdb155d5362f9b43
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-08-04 23:56:40 +0100

    trying to merge

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ import Courses from "./pages/Courses";
 import CourseDetails from "./pages/CourseDetails";
 import Teachers from "./pages/Teachers";
 import NotFound from "./pages/NotFound";
-import AdminLayout from "./components/admin/AdminLayout";
-import Dashboard from "./pages/admin/Dashboard";
-import AdminStudents from "./pages/admin/Students";
-import AdminTeachers from "./pages/admin/Teachers";
-import AdminCourses from "./pages/admin/Courses";
-import AdminLevels from "./pages/admin/Levels";
-import AdminSubjects from "./pages/admin/Subjects";
-import Registrations from "./pages/admin/Registrations";
 
 const queryClient = new QueryClient();
 
@@ -30,17 +22,6 @@ const App = () => (
           <Route path="/courses" element={<Courses />} />
           <Route path="/courses/:id" element={<CourseDetails />} />
           <Route path="/teachers" element={<Teachers />} />
-
-          {/* Admin Routes */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="students" element={<AdminStudents />} />
-            <Route path="teachers" element={<AdminTeachers />} />
-            <Route path="courses" element={<AdminCourses />} />
-            <Route path="levels" element={<AdminLevels />} />
-            <Route path="subjects" element={<AdminSubjects />} />
-            <Route path="registrations" element={<Registrations />} />
-          </Route>
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
